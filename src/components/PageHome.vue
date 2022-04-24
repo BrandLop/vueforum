33d674946724c7f13f1a5a2d29153a733b9752c8
BrandLop <brandon.lopezt@alumno.buap.mx>
2022-04-23 19:54:18 -0500

    Paso3: Agregando PageHome.vue

---
+++ src/components/PageHome.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+		<div v-for="thread in threads" :key="thread.id">
+			<h2>{{thread.title}}</h2>
+		</div>
+	</div>
+</template>
+
+<script>
+import sourceData from "@/data.json";
+
+export default {
+  data() {
+    return {
+			threads: sourceData.threads
+		}
+  },
+};
+</script>
+
+<style></style>
