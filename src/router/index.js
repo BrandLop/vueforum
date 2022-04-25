ed57aa48d87fba68c1c3476fda81ca903aa632e1
BrandLop <brandon.lopezt@alumno.buap.mx>
2022-04-24 21:10:12 -0500

    Paso12: Redirigiendo a PageNotFound sin cambiar la url

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import PageHome from "@/components/PageHome";
 import PageThreadShow from "@/components/PageThreadShow";
 import PageNotFound from "@/components/PageNotFound";
+import sourceData from "@/data.json"
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
@@ -14,6 +15,21 @@ const routes = [
     name: "threadshow",
     component: PageThreadShow,
     props: true,
+		beforeEnter (to,from,next) {
+			const threadExist = sourceData.threads.find(thread => thread.id === to.params.id)
+			if(threadExist){
+				return next()
+			} else {
+				next({
+					name: 'pagenotFound',
+					params: {
+						pathMatch: to.path.substring(1).split('/')
+					},
+					query: to.query,
+					hash: to.hash
+				})
+			}
+		}
   },
   { 
 		path: "/:pathMatch(.*)*", 
