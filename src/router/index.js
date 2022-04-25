e1a0da032440354a9aabdcc3c6411b55f67320ad
BrandLop <brandon.lopezt@alumno.buap.mx>
2022-04-24 20:46:31 -0500

    Paso11: Creando PageNotFound y configurandola en el router

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,28 @@
-import PageHome from '@/components/PageHome'
-import PageThreadShow from '@/components/PageThreadShow'
-import { createRouter, createWebHistory } from 'vue-router'
+import PageHome from "@/components/PageHome";
+import PageThreadShow from "@/components/PageThreadShow";
+import PageNotFound from "@/components/PageNotFound";
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
-    {
-        path: '/',
-        name: 'home',
-        component: PageHome
-    },
-    {
-        path: '/thread/:id',
-        name: 'threadshow',
-        component: PageThreadShow,
-        props:true
-    }
-]
+  {
+    path: "/",
+    name: "home",
+    component: PageHome,
+  },
+  {
+    path: "/thread/:id",
+    name: "threadshow",
+    component: PageThreadShow,
+    props: true,
+  },
+  { 
+		path: "/:pathMatch(.*)*", 
+		name: "pagenotFound", 
+		component: PageNotFound 
+	}
+];
 
 export default createRouter({
-    history: createWebHistory(),
-    routes
-})
+  history: createWebHistory(),
+  routes,
+});
